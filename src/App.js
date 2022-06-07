18b6b0d87120b65df96a0a7fe5b3dab03b10d5ee
Akarapu, Chaitanya <chaitanya.akarapu@theverygroup.com>
2022-06-07 21:51:53 +0100

    change message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Imperial!</h1>
+        <h1>Hi from Imperial!</h1>
       </header>
     </div>
   );
